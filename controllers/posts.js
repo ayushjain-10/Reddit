60b70e6caccf0bbdfdd145b2099d9c24ed06cc38
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-04-21 22:52:25 -0700

    'Users must be logged in to submit a post'

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,30 +3,34 @@ const Post = require('../models/post');
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
+        const currentUser = req.user;
         Post.find({}).lean()
-            .then((posts) => res.render('posts-index', { posts }))
+            .then((posts) => res.render('posts-index', { posts, currentUser }))
             .catch((err) => {
                 console.log(err.message);
             })
     })
 
     app.get('/posts/new', (req, res) => {
-        res.render('posts-new');
+        const currentUser = req.user;
+        res.render('posts-new', { currentUser });
     });
 
     // CREATE
     app.post('/posts/new', (req, res) => {
-        // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
-
-        // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
-        post.save(() => res.redirect('/'));
+        if (req.user) {
+            const post = new Post(req.body);
+                post.save(() => res.redirect('/'));
+        } else {
+            return res.status(401); // UNAUTHORIZED
+        }
     });
 
     // LOOK UP THE POST
     app.get('/posts/:id', (req, res) => {
+        const currentUser = req.user;
         Post.findById(req.params.id).lean().populate('comments')
-            .then((post) => res.render('posts-show', { post }))
+            .then((post) => res.render('posts-show', { post, currentUser }))
             .catch((err) => {
                 console.log(err.message);
             });
@@ -34,8 +38,9 @@ module.exports = (app) => {
 
     // SUBREDDIT
     app.get('/n/:subreddit', (req, res) => {
+        const currentUser = req.user;
         Post.find({ subreddit: req.params.subreddit }).lean()
-            .then((posts) => res.render('posts-index', { posts }))
+            .then((posts) => res.render('posts-index', { posts, currentUser }))
             .catch((err) => {
                 console.log(err);
             });
